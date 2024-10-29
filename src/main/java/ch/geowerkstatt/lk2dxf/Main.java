6736fd5436309779f85a21daa96f4d6715bcd2cc
Dominic Burger <dominic.burger@geowerkstatt.ch>
2024-10-29 15:14:59 +0100

    Setup gradle project

---
+++ src/main/java/ch/geowerkstatt/lk2dxf/Main.java
@@ -0,0 +1,12 @@
+package ch.geowerkstatt.lk2dxf;
+
+public final class Main {
+    private Main() { }
+
+    /**
+     * Application entry point.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
