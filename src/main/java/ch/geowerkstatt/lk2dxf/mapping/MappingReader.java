242c38073af38e7b096de87f41e55557701dab71
Dominic Burger <dominic.burger@geowerkstatt.ch>
2024-11-07 11:49:33 +0100

    Define XTF to DXF layer mappings

---
+++ src/main/java/ch/geowerkstatt/lk2dxf/mapping/MappingReader.java
@@ -0,0 +1,47 @@
+package ch.geowerkstatt.lk2dxf.mapping;
+
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvParser;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public final class MappingReader {
+    private static final String MAPPING_RESOURCE = "/mappings.csv";
+    private static final CsvMapper MAPPER = CsvMapper
+            .builder()
+            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
+            .build();
+
+    private MappingReader() { }
+
+    /**
+     * Reads the XTF to DXF layer mappings from the CSV resource file.
+     *
+     * @return the list of mappings
+     * @throws IOException if an I/O error occurs
+     */
+    public static List<LayerMapping> readMappings() throws IOException {
+        CsvSchema headerSchema = CsvSchema.emptySchema().withHeader();
+        ObjectReader reader = MAPPER
+                .readerFor(LayerMapping.class)
+                .with(headerSchema)
+                .with(CsvParser.Feature.TRIM_SPACES);
+
+        try (
+                InputStream csvStream = openMappingResource();
+                MappingIterator<LayerMapping> it = reader.readValues(csvStream)
+        ) {
+            return it.readAll();
+        }
+    }
+
+    private static InputStream openMappingResource() {
+        return MappingReader.class.getResourceAsStream(MAPPING_RESOURCE);
+    }
+}
---
+++ src/test/java/ch/geowerkstatt/lk2dxf/mapping/MappingReaderTest.java
@@ -0,0 +1,22 @@
+package ch.geowerkstatt.lk2dxf.mapping;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public final class MappingReaderTest {
+    @Test
+    public void readMappings() throws IOException {
+        List<LayerMapping> mappings = MappingReader.readMappings();
+
+        assertTrue(mappings.stream().noneMatch(m -> m.layer().isBlank()), "Layer is required");
+        assertTrue(mappings.stream().noneMatch(m -> m.objectType().isBlank()), "ObjectType is required");
+        assertTrue(mappings.stream().noneMatch(m -> m.geometry().isBlank()), "Geometry is required");
+        assertTrue(mappings.stream().noneMatch(m -> m.mapping().isEmpty()), "Mapping is required");
+        assertEquals("STILLGELEGTE-LEITUNGEN", mappings.getFirst().layer());
+    }
+}
